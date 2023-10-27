9e92ecc568113d9bec535260a1d0ec0945213448
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-10-26 22:33:18 -0400

    Fix drupal_set_message

--- src/functions/messages.php
+++ src/functions/messages.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
-function drupal_set_message(?\Stringable $message = null, string $type = 'status', bool $repeat = true)
+function drupal_set_message(null|string|Stringable $message = null, string $type = 'status', bool $repeat = true)
 {
-    \Drupal::messenger()->addMessage($message, $type, $repeat);
+    $messenger = \Drupal::messenger();
+    if (isset($message)) {
+        $messenger->addMessage($message, $type, $repeat);
+    }
+    return $messenger->all();
 }
