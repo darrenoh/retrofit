8cbd4dac93c7a6881a52181eaedcada0d64f162c
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2023-10-27 08:16:11 -0400

    Fix double escaping

--- src/functions/messages.php
+++ src/functions/messages.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use Drupal\Core\Render\Markup;
+
 function drupal_set_message(null|string|Stringable $message = null, string $type = 'status', bool $repeat = true)
 {
     $messenger = \Drupal::messenger();
     if (isset($message)) {
-        $messenger->addMessage((string) $message, $type, $repeat);
+        $messenger->addMessage(Markup::create($message), $type, $repeat);
     }
     return $messenger->all();
 }
